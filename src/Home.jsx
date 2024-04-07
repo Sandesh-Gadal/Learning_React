af36ae1564cc83991b1e46d0929203f452bada8c
Sandesh Gadal <gadalsandesh123@gmail.com>
2024-04-07 13:22:36 +0545

    useState Hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,39 @@
-function Home(){
-    return(
-        <div>
-            <div>THis is navbar</div>
-            <div>this is hero section</div>
-            <div>This is content section</div>
-            <div>this is article</div>
-            <div>This is Footer</div>
-        </div>
-    )
+import React, { useState } from 'react'
+
+const Home = () => {
+//  console.log(useState({
+//   name : "Sandesh",
+//   Age : 22
+//  }))
+
+// const state = useState("Sandesh")
+// console.log(state)
+// const firstitem = state[0]
+// const Seconditem = state[1]
+
+const count = useState(0)
+// var count = 0// var doesnot get re-render in the UI
+
+// const number = useState (0)
+// const firstitem = number[0]
+// const seconditem = number[1]
+
+const [number , setNumber ] = useState(0)
+const increasecount = () =>{
+ setNumber(number+1)
+}
+const decreasecount = ()=>{
+ setNumber(number-1)
+}
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={increasecount}>+</button>
+      <button onClick={decreasecount}>-</button>
+    </div>
+    
+  )
 }
 
 export default Home
