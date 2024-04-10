66bbefa7a2861bee5372322cbe079d9e432df74a
Sandesh Gadal <gadalsandesh123@gmail.com>
2024-04-10 21:47:39 +0545

    Link Component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useNavigate} from  'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
 const Contact = () => {
   const navigate = useNavigate()
@@ -14,6 +15,12 @@ const Contact = () => {
  <div>
      <div>Contact</div>
     <button onClick={()=>navigate("/posts/1234",{replace:true,state:{id:1234}})}>Go to about</button>
+
+
+   <Link to="https://google.com">
+   <button>Go to google</button>
+   </Link>
+
     {/* replace remove the origin page through which we navigate to another  */}
 
  </div>
